7b0b2959fb2bf41d1011fcf0a8b91a5472e91eb3
Nemanja <necaintruder@gmail.com>
2020-12-08 19:36:41 +0100

    deploy app

--- deploy.sh
+++ deploy.sh
@@ -15,6 +15,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:nemanjap83/vue-game.git master:gh-pages
+git push -f git@github.com:nemanjap83/vue-game.git main:gh-pages
 
 cd -
